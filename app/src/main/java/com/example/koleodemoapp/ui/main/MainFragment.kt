252570bd20c6ed0018d6e70087c3034295b7c931
Patrycja Haraburda <patrycjaharaburda97@gmail.com>
2020-10-11 14:45:54 +0200

    Added search for second destination

--- app/src/main/java/com/example/koleodemoapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/koleodemoapp/ui/main/MainFragment.kt
@@ -27,8 +27,10 @@ class MainFragment : Fragment() {
     lateinit var viewModel: MainViewModel
     private lateinit var binding: MainFragmentBinding
     private lateinit var disposable: Disposable
-    private var destinationList = mutableListOf<Destination>()
-    private lateinit var adapter: ArrayAdapter<Destination>
+    private var firstDestinationList = mutableListOf<Destination>()
+    private var secondDestinationList = mutableListOf<Destination>()
+    private lateinit var firstDestinationAdapter: ArrayAdapter<Destination>
+    private lateinit var secondDestinationAdapter: ArrayAdapter<Destination>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,58 +51,88 @@ class MainFragment : Fragment() {
 
     override fun onStart() {
         super.onStart()
-        adapter = ArrayAdapter(
+        firstDestinationAdapter = ArrayAdapter(
             context!!,
             android.R.layout.simple_list_item_1,
-            destinationList
+            firstDestinationList
+        )
+
+        secondDestinationAdapter = ArrayAdapter(
+            context!!,
+            android.R.layout.simple_list_item_1,
+            secondDestinationList
         )
 
         binding.searchFirstStation.isActivated = true
         binding.searchSecondStation.isActivated = true
 
-        binding.scrollFirstSuggestions.adapter = this.adapter
+        binding.scrollFirstSuggestions.adapter = this.firstDestinationAdapter
+        binding.scrollSecondSuggestions.adapter = this.secondDestinationAdapter
 
-        binding.scrollFirstSuggestions.onItemClickListener =
-            AdapterView.OnItemClickListener { parent: AdapterView<*>?, _: View?, position: Int, _: Long ->
+        val onFirstSearchClickListener = {
+            Timber.d("TEST first search click")
+            if (firstDestinationList.isEmpty()) {
+                disposable = viewModel.getDestinationsLists()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { destinations ->
+                        firstDestinationList = destinations.toMutableList()
+                        firstDestinationList.sortWith(kotlin.Comparator { dest1, dest2 ->
+                            (dest2.hits ?: 0) - (dest1.hits ?: 0)
+                        })
+                        firstDestinationAdapter.addAll(firstDestinationList)
+                    }
+            } else {
+                firstDestinationAdapter.addAll(firstDestinationList)
+            }
+        }
+
+        val onSecondSearchClickListener = {
+            Timber.d("TEST second search click")
+            if (secondDestinationList.isEmpty()) {
+                disposable = viewModel.getDestinationsLists()
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe { destinations ->
+                        secondDestinationList = destinations.toMutableList()
+                        secondDestinationList.sortWith(kotlin.Comparator { dest1, dest2 ->
+                            (dest2.hits ?: 0) - (dest1.hits ?: 0)
+                        })
+                        secondDestinationAdapter.addAll(secondDestinationList)
+                    }
+            } else {
+                secondDestinationAdapter.addAll(secondDestinationList)
+            }
+        }
+
+        val onFirstScrollItemClickListener =
+            AdapterView.OnItemClickListener { parent: AdapterView<*>?,
+                                              _: View?, position: Int, _: Long ->
                 val destinationName = (parent?.getItemAtPosition(position) as Destination).name
                 binding.searchFirstStation.setQuery(destinationName, true)
             }
 
-        binding.searchFirstStation.setOnSearchClickListener {
-            disposable = viewModel.getDestinationsLists()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { destinations ->
-                    destinationList = destinations.toMutableList()
-                    destinationList.sortWith(kotlin.Comparator { dest1, dest2 ->  (dest2.hits ?: 0) - (dest1.hits ?: 0)  })
-                    adapter.addAll(destinationList)
-                }
-        }
+        val onSecondScrollItemClickListener =
+            AdapterView.OnItemClickListener { parent: AdapterView<*>?,
+                                              _: View?, position: Int, _: Long ->
+                val destinationName = (parent?.getItemAtPosition(position) as Destination).name
+                binding.searchSecondStation.setQuery(destinationName, true)
+            }
 
-        val searchCloseButtonId = binding.searchFirstStation.context.resources
-            .getIdentifier("android:id/search_close_btn", null, null)
-        val closeButton = binding.searchFirstStation.findViewById(searchCloseButtonId) as ImageView
-        closeButton.setOnClickListener {
-            adapter.clear()
-            binding.scrollFirstSuggestions.visibility = View.INVISIBLE
-            binding.searchFirstStation.setQuery("", false)
-        }
-
-        binding.searchFirstStation.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        val onFirstSearchQueryTextListener = object : SearchView.OnQueryTextListener {
             override fun onQueryTextChange(newText: String?): Boolean {
-                adapter.filter.filter(newText)
+                firstDestinationAdapter.filter.filter(newText)
                 binding.scrollFirstSuggestions.visibility = View.VISIBLE
-                if(adapter.isEmpty) {
-                    adapter.addAll(destinationList)
+                if (firstDestinationAdapter.isEmpty) {
+                    firstDestinationAdapter.addAll(firstDestinationList)
                 }
                 return false
             }
 
             override fun onQueryTextSubmit(query: String?): Boolean {
-                val destinationNames = destinationList.map { it.name?: "" }
+                val destinationNames = firstDestinationList.map { it.name ?: "" }
                 val destination = destinationNames.find { it.contains(query ?: "", true) }
                 if (destination != null) {
                     binding.searchFirstStation.setQuery(destination, false)
-                    adapter.clear()
+                    firstDestinationAdapter.clear()
                     binding.scrollFirstSuggestions.visibility = View.INVISIBLE
 
                 } else {
@@ -108,13 +140,74 @@ class MainFragment : Fragment() {
                 }
                 return false
             }
-        })
+        }
 
+        val onSecondSearchQueryTextListener = object : SearchView.OnQueryTextListener {
+            override fun onQueryTextChange(newText: String?): Boolean {
+                secondDestinationAdapter.filter.filter(newText)
+                binding.scrollSecondSuggestions.visibility = View.VISIBLE
+                if (secondDestinationAdapter.isEmpty) {
+                    secondDestinationAdapter.addAll(secondDestinationList)
+                }
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val destinationNames = secondDestinationList.map { it.name ?: "" }
+                val destination = destinationNames.find { it.contains(query ?: "", true) }
+                if (destination != null) {
+                    binding.searchSecondStation.setQuery(destination, false)
+                    secondDestinationAdapter.clear()
+                    binding.scrollSecondSuggestions.visibility = View.INVISIBLE
+
+                } else {
+                    Toast.makeText(context, "No Match found", Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+        }
+
+        val onFirstSearchCloseClickListener = {
+            firstDestinationAdapter.clear()
+            binding.scrollFirstSuggestions.visibility = View.INVISIBLE
+            binding.searchFirstStation.setQuery("", false)
+        }
+
+        val onSecondSearchCloseClickListener = {
+            secondDestinationAdapter.clear()
+            binding.scrollSecondSuggestions.visibility = View.INVISIBLE
+            binding.searchSecondStation.setQuery("", false)
+        }
+
+        binding.searchFirstStation.setOnSearchClickListener {
+            onFirstSearchClickListener.invoke()
+        }
+        binding.searchFirstStation.setOnQueryTextListener(onFirstSearchQueryTextListener)
+        binding.scrollFirstSuggestions.onItemClickListener = onFirstScrollItemClickListener
+
+        val searchCloseFirstButtonId = binding.searchFirstStation.context.resources
+            .getIdentifier("android:id/search_close_btn", null, null)
+        val firstCloseButton = binding.searchFirstStation.findViewById(searchCloseFirstButtonId) as ImageView
+        firstCloseButton.setOnClickListener {
+            onFirstSearchCloseClickListener.invoke()
+        }
+
+        binding.searchSecondStation.setOnSearchClickListener {
+            onSecondSearchClickListener.invoke()
+        }
+        binding.searchSecondStation.setOnQueryTextListener(onSecondSearchQueryTextListener)
+        binding.scrollSecondSuggestions.onItemClickListener = onSecondScrollItemClickListener
+
+        val searchCloseSecondButtonId = binding.searchSecondStation.context.resources
+            .getIdentifier("android:id/search_close_btn", null, null)
+        val secondCloseButton = binding.searchSecondStation.findViewById(searchCloseSecondButtonId) as ImageView
+        secondCloseButton.setOnClickListener {
+            onSecondSearchCloseClickListener.invoke()
+        }
     }
 
     override fun onDestroy() {
         disposable.dispose()
         super.onDestroy()
     }
-
 }
